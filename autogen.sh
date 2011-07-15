e1b3f7fe282548f287d8847a39d663fa08854b27
HuangCongyu <huangcongyu2006@gmail.com>
2011-07-15 23:17:54 +0800

    整理文件

--- autogen.sh
+++ autogen.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-echo "aclocal..."
-aclocal
-echo "autoconf..."
-autoconf
-echo "autoheader..."
-autoheader
-echo "automake..."
-automake -a
-./configure --enable-debug --prefix=/home/hcy/tmp/gtkqq
+autoreconf --install
