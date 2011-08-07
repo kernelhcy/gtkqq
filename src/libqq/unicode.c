739e218fb20f94b02f7543ec7280207418648ea5
HuangCongyu <huangcongyu2006@gmail.com>
2011-08-07 15:10:48 +0800

    修复utf16转utf8的bug

--- src/libqq/unicode.c
+++ src/libqq/unicode.c
@@ -45,9 +45,12 @@ static void do_ucs4toutf8(GString *to, const gchar *from)
     static guint T = 0x02;
     static guint sep1 = 0x80;
     static guint sep2 = 0x800;
+    static guint sep3 = 0x10000;
+    static guint sep4 = 0x200000;
+    static guint sep5 = 0x4000000;
         
     guint tmp[4];
-    gchar re[3];
+    gchar re[6];
     guint ivalue = 0;
         
     gint i;
@@ -60,13 +63,15 @@ static void do_ucs4toutf8(GString *to, const gchar *from)
     //decode
    if(ivalue < sep1){
         //0xxxxxxx
+        re[0] = 0x7f & tmp[3];
+        g_string_append_c(to, re[0]);
    }else if(ivalue < sep2){
         //110xxxxx 10xxxxxx
         re[0] = (0x3 << 6) | ((tmp[1] & 7) << 2) | (tmp[2] >> 2);
         re[1] = (0x1 << 7) | ((tmp[2] & 3) << 4) | tmp[3];
         g_string_append_c(to, re[0]);
         g_string_append_c(to, re[1]);
-    }else{
+    }else if(ivalue < sep3){
         //1110xxxx 10xxxxxx 10xxxxxx
         re[0] = E | (tmp[0] & 0xf);
         re[1] = (T << 6) | (tmp[1] << 2) | ((tmp[2] >> 2) & 0x3);
@@ -75,6 +80,24 @@ static void do_ucs4toutf8(GString *to, const gchar *from)
         for(i = 0; i < 3; ++i){
             g_string_append_c(to, re[i]);
         }
+    }else if(ivalue < sep4){
+        //11110xxx 10xxxxxx 10xxxxxx 10xxxxxx
+        re[0] = 0xf0 | ((tmp[1] >> 2) & 0x7);
+        re[1] = 0x2 << 6 | ((tmp[1] & 0x3) << 4) | ((tmp[2] >> 4) & 0xf);
+        re[2] = 0x2 << 6 | ((tmp[2] & 0xf) << 4) | ((tmp[3] >> 6) & 0x3);
+        re[3] = 0x2 << 6 | (tmp[2] & 0x3f);
+        //copy to @to.
+        for(i = 0; i < 4; ++i){
+            g_string_append_c(to, re[i]);
+        }
+    }else if(ivalue < sep5){
+        //111110xx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
+        g_warning("Not implement Unicoe convert!! U+00200000–U+03FFFFFF "
+                        "(%s, %d)", __FILE__, __LINE__);
+    }else{
+        //1111110x 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx
+        g_warning("Not implement Unicoe convert!! U+04000000–U+7FFFFFFF "
+                        "(%s, %d)", __FILE__, __LINE__);
     }
 }
 
