f39d6202a03a8a50637ba9cbff0a5ff6b0e7f9e4
devil wang <devil@sabayon.(none)>
2011-11-21 05:33:28 +0800

    wxjeacen modify deploy script

--- autogen.sh
+++ autogen.sh
@@ -3,4 +3,7 @@ set -x
 aclocal 					\
 && autoconf					\
 && libtoolize 	--copy --force --automake 	\
-&& autoreconf --install
+&& autoreconf --install				\
+&& [ -d usr ] || mkdir usr			\
+&& ./configure --prefix=`pwd`/usr/
+
