363badd4ae0fdfe9fa7b1ad97905c89ed6c1126f
mathslinux <riegamaths@gmail.com>
2011-12-02 23:59:37 +0800

    Support sound notifications.

--- src/gui/msgdispacher.c
+++ src/gui/msgdispacher.c
@@ -4,6 +4,8 @@
 #include <groupchatwindow.h>
 #include <tray.h>
 #include <msgloop.h>
+#include <config.h>
+#include <sound.h>
 
 //
 // Global 
@@ -12,10 +14,45 @@ extern QQTray *tray;
 extern QQInfo *info;
 extern GQQConfig *cfg;
 
+#ifdef USE_GSTREAMER
+static void qq_sound_notify(QQRecvMsg *msg)
+{
+	char filename[512] = {0};
+	
+	if (!msg)
+		return ;
+	
+	switch (msg->msg_type) {
+    case MSG_BUDDY_T:
+    case MSG_GROUP_T:
+		/* When new message is coming, play a msg audio. */ 
+		g_snprintf(filename, sizeof(filename), SOUNDDIR"/Classic/msg.wav");
+        break;
+    case MSG_STATUS_CHANGED_T:
+		if (!g_strcmp0(msg->status->str, "online")) {
+			g_snprintf(filename, sizeof(filename), SOUNDDIR"/Classic/Global.wav");
+			break;
+		} else {
+			return;
+		}
+    case MSG_KICK_T:
+		/* No implement now */
+		return;
+    default:
+        g_warning("Unknonw poll message type! %d (%s, %d)", msg->msg_type
+				  , __FILE__, __LINE__);
+		return;
+    }
+	
+	qq_play_wavfile(filename);
+}
+#endif //USE_GSTREAMER
+
 static void qq_poll_dispatch_buddy_msg(QQRecvMsg *msg)
 {
     GtkWidget *cw = gqq_config_lookup_ht(cfg, "chat_window_map"
                                             , msg -> from_uin -> str);
+	
     if(cw == NULL){
         cw = qq_chatwindow_new(msg -> from_uin -> str); 
         // not show it
@@ -62,6 +99,12 @@ gint qq_poll_message_callback(QQRecvMsg *msg, gpointer data)
     }
    
     GQQMessageLoop *loop = data;
+
+#ifdef USE_GSTREAMER
+	/* Play a audio to notify that a new msg is coming. */
+	qq_sound_notify(msg);
+#endif // USE_GSTREAMER
+	
     switch(msg -> msg_type)
     {
     case MSG_BUDDY_T:
