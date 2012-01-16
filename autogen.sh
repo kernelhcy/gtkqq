52cd4dbf1364ede66bdd063284bd0b50a2923822
billzt <billzt@hotmail.com>
2012-01-16 19:56:48 +0800

    让用户自己设定编译参数，不直接在 autogen.sh 里面制定

--- autogen.sh
+++ autogen.sh
@@ -3,6 +3,4 @@ set -x
 aclocal 					\
 && autoconf					\
 && libtoolize 	--copy --force --automake 	\
-&& autoreconf --install				\
-&& [ -d usr ] || mkdir usr			\
-&& ./configure --enable-debug --prefix=`pwd`/usr
+&& autoreconf --install
