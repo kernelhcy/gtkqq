08251776e71eeb2a2d561588ade20a7f3fed4f6c
Mike Manilone <crtmike@gmail.com>
2012-02-24 14:13:31 +0800

    fixed g_thread_init

--- src/libqq/qqinit.c
+++ src/libqq/qqinit.c
@@ -6,10 +6,18 @@
 
 QQInfo* qq_init(GError **err)
 {
+#if !GLIB_CHECK_VERSION(2,32,0)
+    /* g_thread_init and g_thread_supported has been deprecated
+       since version 2.32. 
+       These functions are no longer necessary. The GLib threading system is automatically initialized at the start of your program.
+    */
     if(!g_thread_supported()){
         g_thread_init(NULL);
         g_message("Init the gthread system. (%s, %d)", __FILE__, __LINE__);
     }
+#else
+    g_message("gthread auto initialized. (%s, %d)", __FILE__, __LINE__);
+#endif
     QQInfo *info = qq_info_new();
     return info;
 }
